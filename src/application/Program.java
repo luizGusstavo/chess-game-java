b20972b59bd73aaf2c37ca8384a494b1256328d6
lgusstavo0112 <luizgs@ufba.br>
2021-06-11 14:48:24 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,12 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/C/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			} 
 			catch (ChessException excecao) {
 				System.out.print(excecao.getMessage());
